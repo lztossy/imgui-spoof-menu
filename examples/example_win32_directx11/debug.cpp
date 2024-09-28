a27ee8c41aed6954a3361841493622a4c730e5c2
tos <164348607+lztossy@users.noreply.github.com>
2024-09-28 18:58:27 +1000

    Add files via upload

---
+++ examples/example_win32_directx11/debug.cpp
@@ -0,0 +1,10 @@
+#define IMSPINNER_DEMO
+#include "imspinner.h"
+
+int main(int, char**) {
+    ...
+        while (!done) {
+            ...
+                ImSpinner::demoSpinners();
+        }
+}
